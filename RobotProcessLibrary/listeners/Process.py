de6f7d9c06e4c0410542207c3eadccf2b387e0b0
Andras Kelemen <kelemenandras11@gmail.com>
2019-05-08 19:47:51 +0200

    setup commands

--- RobotProcessLibrary/listeners/Process.py
+++ RobotProcessLibrary/listeners/Process.py
@@ -3,7 +3,6 @@
 
 from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
 import os
-import re
 
 
 class Process(object):
@@ -15,18 +14,20 @@ class Process(object):
 	def output_file(self, path):
 		self.output_source = path
 
+	def output_file(self, path):
+		self.output_source = path
+
 	def log_file(self, path):
 		log_source = path
 		f = open(log_source, 'r')
-		filedata = f.readlines()
+		file_data = f.readlines()
 		f.close()
 
 		f = open(log_source, 'w')
 		line_no = 0
-		for line in filedata:
+		for line in file_data:
 			line = str(line)
 
-			#TODO haetekonyan, mert valami csak egyszer van
 			try:
 				"." in line
 			except UnicodeDecodeError:
@@ -34,48 +35,28 @@ class Process(object):
 				line_no += 1
 				continue
 			if "<h2>Test Execution Log</h2>" in line:
-				print("csere1", line_no)
 				line = line.replace("<h2>Test Execution Log</h2>", "<h2>Process Execution Log</h2>")
 			elif 'givenTitle : suiteName + " Test " + type' in line:
-				print("csere2", line_no)
 				line = line.replace('givenTitle : suiteName + " Test " + type', 'givenTitle : suiteName + " Process " + type')
 			elif "Test Statistics" in line:
-				print("csere3", line_no)
 				line = ""
-				filedata[line_no + 1] = ""
-				filedata[line_no + 2] = ""
+				file_data[line_no + 1] = ""
+				file_data[line_no + 2] = ""
 			elif '<span class="label ${status.toLowerCase()}">TEST</span>' in line:
 				line = line.replace("TEST", "SUBPROCESS")
-				print("csere4", line_no)
 			elif '<span class="label ${status.toLowerCase()}">SUITE</span>' in line:
 				line = line.replace("SUITE", "PROCESS")
-				print("csere5", line_no)
-
 			elif "${critical} critical test," in line:
 				line = line.replace("critical test", "process")
-				print("csere6", line_no)
 			elif "${total} test total," in line:
 				line = line.replace("test", "process")
-				print("csere7", line_no)
 			elif ">Statistics by Suite</th>" in line:
 				line = line.replace("Suite", "Process")
-				print("csere8", line_no)
-
-
+			elif "window.output" in line and ("_.txt" in line or "_.robot" in line):
+				line = line.replace("_.txt", ".txt").replace("_.robot", ".robot")
 
 			f.write(line)
 			line_no += 1
-
-		#newdata = re.sub(r".*\n.*Total Statistics.*\n.*\n", "", filedata)
-		#if filedata.find(r".*\r\n.*(Total Statistics).*\r\n.*\r\n") != -1:
-		#    print('true')
-		#    print(filedata.index(r".*\r\n.*(Total Statistics).*\r\n.*\r\n"))
-		#else:
-		#    print('false')
-
-		#'<table class="statistics" id="total-stats"><thead><tr>' +
-		#'<th class="stats-col-name">Total Statistics</th>' + statHeaders +
-		#'</tr></thead></table>' +
 
 		f.close()
 		os.remove(self.output_source)
--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 import setuptools
 from setuptools.command.install import install
+import os
+import sys
+
 
 class PostInstallCommand(install):
 	"""Post-installation for installation mode."""
 	def run(self):
-		import os, sys
 		scripts = os.path.join(os.path.dirname(sys.executable), "Scripts")
-
 		bat_file = os.path.join(scripts, "roboproc.bat")
 		write_file = open(bat_file, 'w')
 		write_file.write("@echo off\n")
+		write_file.write("set process=%process*=%\n")
+		write_file.write("set process=%*\n")
+		write_file.write("set process=%process:.robot=_.robot%\n")
+		write_file.write("set process=%process:.txt=_.txt%\n")
 		write_file.write("set batpath=%~dp0\n")
 		write_file.write("set listenerpath=%batpath%\..\Lib\site-packages\RobotProcessLibrary\listeners\Process.py\n")
 		write_file.write("set totestpath=%batpath%\..\Lib\site-packages\RobotProcessLibrary\listeners\\to_test.py\n")
 		write_file.write("set toprocesspath=%batpath%\..\Lib\site-packages\RobotProcessLibrary\listeners\\to_process.py\n")
 		write_file.write("python %totestpath% %*\n")
-		write_file.write("python -m robot.run -r None --listener %listenerpath% %*\n")
+		write_file.write("python -m robot.run -r None --listener %listenerpath% %process%\n")
 		write_file.write("python %toprocesspath% %*\n")
 		write_file.close()
 		install.run(self)
@@ -31,8 +36,11 @@ setuptools.setup(name='robotprocesslibrary',
       packages=setuptools.find_packages(exclude=['RobotProcessLibrary.tests']),
 
       install_requires=[
-      # todo a 3 lib
         'robotframework',
+        'EmailProcessLibrary',
+        'OsProcessLibrary',
+        'ExcelProcessLibrary',
+        'OcrProcessLibrary',
       ],
       cmdclass={
         'install': PostInstallCommand,
